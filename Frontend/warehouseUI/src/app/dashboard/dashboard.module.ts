14a9ec9d845f1ee3468bde08053cebee0f6e3a67
tejaenjam <enjamdharmateja1997@gmail.com>
2023-11-19 22:09:08 -0500

    dashboard

---
+++ Frontend/warehouseUI/src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DemoFlexyModule } from '../demo-flexy-module'
+import { DashboardComponent } from './dashboard.component';
+import { SalesComponent } from './dashboard-components/sales/sales.component';
+import { ActivityComponent } from './dashboard-components/activity/activity.component';
+import { ProductComponent } from './dashboard-components/product/product.component';
+import { CardsComponent } from './dashboard-components/cards/cards.component';
+import { FormsModule } from '@angular/forms';
+import { NgApexchartsModule } from 'ng-apexcharts';
+
+// icons
+import { TablerIconsModule } from 'angular-tabler-icons';
+import * as TablerIcons from 'angular-tabler-icons/icons';
+
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    SalesComponent,
+    ActivityComponent,
+    ProductComponent,
+    CardsComponent,
+  ],
+  imports: [
+    CommonModule,
+    DemoFlexyModule,
+    FormsModule,
+    TablerIconsModule.pick(TablerIcons),
+    NgApexchartsModule
+  ],
+  exports: [
+    DashboardComponent,
+    SalesComponent,
+    ActivityComponent,
+    ProductComponent,
+  ]
+})
+export class DashboardModule { }
